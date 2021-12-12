0de7f243bf139da1ac8ed523768ee1ca4257b6e4
0ojixueseno0 <jz0ojiang@qq.com>
2021-12-12 17:31:00 +0800

    first commit:what2eat?

---
+++ README.md
@@ -0,0 +1,26 @@
+![logo.png](/static/logo.png)
+
+<center><h1>吃点啥？</h1></center>
+
+---
+
+<center><image src="https://img.shields.io/static/v1?label=Organization&message=TrashStation&color=lightgrey&logo=github"></image> | <image src="https://img.shields.io/static/v1?label=Vercel&message=Pages&color=green&logo=vercel"></image> | <image src="https://img.shields.io/static/v1?label=Author&message=Jz0ojiang&color=9cf&logo=github"></image></center>
+
+犹豫不决吃什么？前往 [吃点啥]() 看看有什么好吃的！
+
+> 图片素材来源：
+>
+> 食物图片部分来自 [pcrbot/whattoeat: 今天吃点啥 (github.com)](https://github.com/pcrbot/whattoeat)
+>
+> 商铺图片部分来自 美团，饿了么 商铺头像
+>
+> Logo 来源：John Frank Wayre/Helen Rae Cooper
+
+## 食物太少？
+
+Q: 如何增加更多美食？
+
+A: 将食物图片以名称进行命名，直接放置在foods文件夹中即可
+
+**欢迎提交PR丰富美食列表**
+
---
+++ static/listdata.py
@@ -0,0 +1,45 @@
+import os
+import json
+
+# 读取 foods 文件夹下的所有文件，并将其名称和路径存入字典中
+
+
+def get_foods():
+    foods = {}
+    foods["list"] = []
+    for file in os.listdir('static/foods'):
+        foods["list"].append(".".join(file.split('.')[:-1]))
+        foods[".".join(file.split('.')[:-1])] = 'static/foods/' + file
+    return foods
+
+# 读取 shops/meituan 和 shops/ele 文件夹下的所有文件，并将其名称和路径存入字典中
+
+
+def get_shops():
+    shops = {}
+    shops['meituan'] = []
+    shops['ele'] = []
+    shops["detail"] = {}
+    shops["detail"]["meituan"] = {}
+    shops["detail"]["ele"] = {}
+    for file in os.listdir('static/shops/meituan'):
+        shops["meituan"].append(".".join(file.split('.')[:-1]))
+        shops["detail"]["meituan"][".".join(file.split(
+            '.')[:-1])] = 'static/shops/meituan/' + file
+    for file in os.listdir('static/shops/ele'):
+        shops["ele"].append(".".join(file.split('.')[:-1]))
+        shops["detail"]["ele"][".".join(file.split(
+            '.')[:-1])] = 'static/shops/ele/' + file
+    return shops
+
+
+def main():
+    foods = get_foods()
+    shops = get_shops()
+    jsonfile = {"foods": foods, "shops": shops}
+    with open('static/listdata.json', 'w', encoding="utf-8") as f:
+        json.dump(jsonfile, f, indent=4, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    main()
